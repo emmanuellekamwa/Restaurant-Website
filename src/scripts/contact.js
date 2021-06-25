87aff8399c6b3cdd055785d6b8d10e91bdc25e50
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-06-25 12:36:20 +0100

    Fix eslint errors

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,26 +1,26 @@
 const addContactsContent = () => {
-    const contactsContent = document.createElement("div");
+  const contactsContent = document.createElement('div');
 
-    contactsContent.classList.add("contacts-info-div");
+  contactsContent.classList.add('contacts-info-div');
 
-    const li = document.createElement("li");
-    li.textContent = "Email: emmanuellekamwa@gmail.com";
-    contactsContent.appendChild(li);
+  const li = document.createElement('li');
+  li.textContent = 'Email: emmanuellekamwa@gmail.com';
+  contactsContent.appendChild(li);
 
-    return contactsContent;
+  return contactsContent;
 };
 
 const createContactsDiv = () => {
-    const contactsDiv = document.createElement("div");
-    contactsDiv.classList.add("contacts-div");
+  const contactsDiv = document.createElement('div');
+  contactsDiv.classList.add('contacts-div');
 
-    const heading = document.createElement("h1");
-    heading.textContent = "Contact Us";
+  const heading = document.createElement('h1');
+  heading.textContent = 'Contact Us';
 
-    contactsDiv.appendChild(heading);
-    contactsDiv.appendChild(addContactsContent());
+  contactsDiv.appendChild(heading);
+  contactsDiv.appendChild(addContactsContent());
 
-    return contactsDiv;
+  return contactsDiv;
 };
 
 export default createContactsDiv;
--- src/scripts/loadingpage.js
+++ src/scripts/loadingpage.js
@@ -1,17 +1,17 @@
-const loadInitialPage = (title, myCaption = "") => {
-    const infoDiv = document.createElement("div");
-    infoDiv.classList.add("info-div");
+const loadInitialPage = (title, myCaption = '') => {
+  const infoDiv = document.createElement('div');
+  infoDiv.classList.add('info-div');
 
-    const heading = document.createElement("h1");
-    heading.textContent = title;
+  const heading = document.createElement('h1');
+  heading.textContent = title;
 
-    const paragraph = document.createElement("p");
-    paragraph.textContent = myCaption;
+  const paragraph = document.createElement('p');
+  paragraph.textContent = myCaption;
 
-    infoDiv.appendChild(heading);
-    infoDiv.appendChild(paragraph);
+  infoDiv.appendChild(heading);
+  infoDiv.appendChild(paragraph);
 
-    return infoDiv;
+  return infoDiv;
 };
 
 export default loadInitialPage;
